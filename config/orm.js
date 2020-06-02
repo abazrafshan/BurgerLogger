32076bf32a2063e6421567d28e181a47e9df800c
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-06-01 19:59:52 -0700

    Built basic framework for files

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+var connection = require("./connection.js");
+
+// create the methods that will execute the 
+// necessary MySQL commands in the controllers. 
+// These are the methods you will need to use in 
+// order to retrieve and store data in your database
+
+var orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function(){
+
+    }
+};
+
+module.exports = orm;
